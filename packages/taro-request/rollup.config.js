3720ffa32fc6e80fe3e4f2f3494ea89e1690335b
liangsky <640634387@qq.com>
2025-12-14 19:18:53 +0800

    feat!: only support esm format,build form rollup to rolldown

--- packages/taro-request/rollup.config.js
+++
@@ -1,8 +0,0 @@
-import { createRequire } from 'node:module';
-import { defineConfig } from 'rollup';
-import { getConfig } from '../rollup.base.config.js';
-
-const require = createRequire(import.meta.url);
-const packageJSON = require('./package.json');
-
-export default defineConfig([getConfig(packageJSON)]);
